a6724e4b5f8fbc1805b2d251f93f19049fb452b9
VishalSajjan1996 <117075426+VishalSajjan1996@users.noreply.github.com>
2023-01-23 15:11:58 +0530

    Add files via upload

---
+++ week1_23Jan2023_to_27Jan2023/23Jan2023/qODEA/odea/index.php
@@ -0,0 +1,215 @@
+<?php
+    $title = "qODEA | On Demand Enterprise Automation";
+    require_once('common_files/header.php');
+?>
+
+<header class="secHeaderContainer">
+    <div class="sectionHeader">
+        <ul>
+            <li><a href="https://www.qualesce.com/"><img src="assets/img/Logo.svg"></a></li>
+            <li><a href="#" class="myAccount">My Account</a></li>
+        </ul>
+    </div>
+</header>
+
+<section class="sec1Container">
+    <article class="sec1Art1Container">
+        <div class="sec1A1Wrapper">
+            <h2>Customer Hurdles To Adapt <br>Automation</h2>
+            <ul class="adaptUl">
+                <div>
+                    <li>Do not have big budget</li>
+                    <li>Do not want to commit to minimum licenses</li>
+                    <li>Do not want long term commitment</li>
+                </div>
+                <div>
+                    <li>Do not have pro-services resources</li>
+                    <li>Want to prove the ROI of automation</li>
+                    <li>Complex business processes</li>
+                </div>
+            </ul>
+        </div>
+    </article>
+    <article class="sec1Art2Container">
+        <div class="sec1A2Wrapper">
+            <h2>ODEA</h2>
+            <p>On Demand Enterprise Automation</p>
+        </div>
+    </article>
+</section>
+
+<section class="sec2Container">
+    <article class="sec2Art1Container">
+        <div class="sec2A1Wrapper">
+            <h2>ODEA - True Monthly Subscription Model<sup>*</sup></h2>
+           
+<table id="customers">
+ <tr class="row1">
+ <th></th>
+<th>100 hours</th>
+ <th>60 hours</th>
+ <th>40 hours</th>
+ <th>40 hours</th>
+ <th>20 hours</th> </tr>
+ <tr class="row2">
+ <th style="background: white;"></th>
+ <th class="std">PLATINUM</th>
+ <th class="std">GOLD</th>
+ <th class="std">POC/POV</th>
+ <th class="std">EVERGREEN</th>
+ <th class="std">BASE</th>
+ </tr>
+ <tr>
+ <td class="clr"><strong> qBOT</strong></td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+</tr>
+<tr>
+ <td class="clr"><strong>Architecture</strong> </td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td></td>
+ <td></td>
+</tr>
+<tr>
+ <td class="clr"><strong>Process Intelligence</strong> </td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td class="tick">✅</td>
+ <td></td>
+ <td></td>
+ </tr>
+ <tr>
+ <td class="clr"><strong> Development</strong></td>
+ <td class="tick">✅</td>
+ <td></td>
+ <td class="tick">✅</td>
+ <td></td>
+ <td></td>
+ </tr>
+ <tr>
+ <td class="clr"><strong>Run and Maintain</strong> </td>
+ <td class="tick">✅</td>
+ <td></td>
+ <td></td>
+ <td class="tick">✅</td>
+ <td></td>
+</tr>
+
+</table>
+ </div>
+ </article>
+
+
+        </div>
+    </article>
+    <article class="sec2Art2Container">
+            <div class="sec2A2Wrapper">
+                <h2>Packages</h2>
+                <div class="sec2Cards">
+                <div>
+                    <h3>Base</h3>
+                    <h4><strong>$995</strong></h4>
+                </div>
+                <div>
+                    <h3>Evergreen</h3>
+                    <h4><strong>$1,495</strong></h4>
+                </div>
+                <div>
+                    <h3>POC/POV</h3>
+                    <h4><strong>$1,495</strong></h4>
+                </div>
+                <div>
+                    <h3>Gold</h3>
+                    <h4><strong>$2,495</strong></h4>
+                </div>
+                <div>
+                    <h3>Platinum</h3>
+                    <h4><strong>$3,495</strong></h4>
+                </div>
+                </div>
+            </div>
+        </article>
+    </section>
+
+    <section class="sec3Container">
+        <article class="sec3Art1Container">
+            <div class="sec3A1Wrapper">
+                    <h2>Automation Journey</h2>
+                    <div class="sec3cards">
+                <div class="sec3Card">
+                    <h4>POC/POV</h4>
+                    <div>
+                        <p>Automation Architecture</p>
+                        <p>+</p>
+                        <p>Process Intelligence</p>
+                    </div>
+                    
+                </div>
+                <img src="assets/img/arrow.svg" alt="arrowIcon">
+                <div class="sec3Card">
+                    <h4>Gold</h4>
+                    <div>
+                        <p>Automate Processes</p>
+                        <p>+</p>
+                        <p>New Process Queue</p>
+                    </div>
+                </div>
+                <img src="assets/img/arrow.svg" alt="arrowIcon">
+                <div class="sec3Card">
+                    <h4>Platinum</h4>
+                    <div>
+                        <p>Run/Maintain</p>
+                        <p>+</p>
+                        <p>Automate</p>
+                        <p>+</p>
+                        <p>New Process</p>
+                    </div>
+                </div>
+                <img src="assets/img/arrow.svg" alt="arrowIcon">
+                <div  class="sec3Card">
+                    <h4>Evergreen</h4>
+                    <div>
+                        <p>Run/Maintain (no more processes to automate)</p>
+                    </div>
+                </div>
+                    </div>
+            </div>
+        </article>
+    </section>
+
+<section class="sec4Container">
+
+    <article class="sec4Art1Container">
+
+        <div class="sec4A1Wrapper">
+
+            <h2>Terms and Conditions</h2>
+
+            <ul class="terms_condition">
+                <li><span>1</span>3 months minimum on termination of convenience.</li>
+                <li><span>2</span>Each package has only one qBot license.</li>
+                <li><span>3</span>Evergreen package has 40 hours of run and maintain services.</li>
+                <li><span>4</span>Gold package has 60 hours of pro-services.</li>
+                <li><span>5</span>Platinum package has 100 hours of pro-services.</li>
+                <li><span>6</span>Any additional service hours above the amount included in the packages are at
+                    $29/hour.</li>
+                <li><span>7</span>Evergreen package has to have development done by Qualesce or a best practices audit
+                    done by Qualesce.</li>
+                <li><span>8</span>Unused service hours cannot be rolled over.</li>
+                <li><span>9</span>Additional 10% off for 1 year commit (non cancellable and paid up-front)</li>
+                <li><span>10</span>Invoice will be issued at the beginning of the month.</li>
+                <li><span>11</span>Net 30 payment terms.</li>
+            </ul>
+        </div>
+    </article>
+
+</section>
+
+<?php
+    require_once('common_files/footer.php');
+?>
